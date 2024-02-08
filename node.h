6af3099e9286318aae9c255ccef12c55d5135992
Yonatan Reicher <yony252525@gmail.com>
2024-02-08 22:56:33 +0200

    Nodes now cache height and count

--- node.h
+++ node.h
@@ -9,31 +9,49 @@ struct Node;
 // These are functions that cannot be methods because they can be called on a nullptr
 
 template <class T>
-int height(Node<T>* root) {
+int height(const Node<T>* root) {
     if (root == nullptr) {
         return 0;
     }
-    return 1 + std::max(height(root->left.get()), height(root->right.get()));
+    return root->m_height;
 }
 
 template <class T>
-int count(Node<T>* root) {
+int count(const Node<T>* root) {
     if (root == nullptr) {
         return 0;
     }
-    return 1 + count(root->left.get()) + count(root->right.get());
+    return root->m_count;
+}
+
+template <class T>
+int balance(const Node<T>* root) {
+    if (root == nullptr) {
+        return 0;
+    }
+    return height(root->m_left.get()) - height(root->m_right.get());
 }
 
 template <class T>
 struct Node {
+    std::unique_ptr<Node> m_left;
+    std::unique_ptr<Node> m_right;
+    int m_height;
+    int m_count;
     int key;
     T data;
-    std::unique_ptr<Node> left;
-    std::unique_ptr<Node> right;
-    //Node* parent;
 
-    int balance() {
-        return height(left.get()) - height(right.get());
+    Node(int key, T data) {
+        key = key;
+        data = std::move(data);
+        m_left = nullptr;
+        m_right = nullptr;
+        updateHeightAndCount();
+    }
+
+    void updateHeightAndCount() {
+        m_height = 1 + std::max(height(m_left.get()), height(m_right.get()));
+        m_count = 1 + count(m_left.get()) + count(m_right.get());
     }
 };
 
