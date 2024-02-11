7bad83c5c0a424f2680f84df4a3a08f645d218fd
Yonatan Reicher <yony252525@gmail.com>
2024-02-11 10:41:15 +0200

    Fixed a bug where node constructor did not save data, and other small things

--- node.h
+++ node.h
@@ -43,8 +43,8 @@ public:
     T data;
 
     Node(int key, T data) {
-        key = key;
-        data = std::move(data);
+        this->key = key;
+        this->data = std::move(data);
         m_left = nullptr;
         m_right = nullptr;
         updateHeightAndCount();
@@ -54,14 +54,14 @@ public:
         std::unique_ptr<Node> ret = std::move(m_left);
         m_left = std::move(newLeft);
         updateHeightAndCount();
-        return std::move(ret);
+        return ret;
     }
 
     std::unique_ptr<Node> setRight(std::unique_ptr<Node> newRight) {
         std::unique_ptr<Node> ret = std::move(m_right);
         m_right = std::move(newRight);
         updateHeightAndCount();
-        return std::move(ret);
+        return ret;
     }
 
     std::unique_ptr<Node> popLeft() {
@@ -86,8 +86,10 @@ private:
         m_count = 1 + count(m_left.get()) + count(m_right.get());
     }
 
-    friend int height(const Node<T>*);
-    friend int count(const Node<T>*);
+    template <class U>
+    friend int height(const Node<U>*);
+    template <class U>
+    friend int count(const Node<U>*);
 };
 
 #endif
