3cb663d8e0177a073df529a258e6bdd2724fb399
Yonatan Reicher <yony252525@gmail.com>
2024-02-24 17:09:59 +0200

    Team now uses StrengthAndId as Tree key

--- Team.h
+++ Team.h
@@ -31,8 +31,8 @@ public:
     Country* m_country;
     int m_strength;
     int m_points;
-    Tree<Node<int>> m_contestantIds[NUM_OF_TREES];
-    Tree<Node<int>> m_contestantPowers[NUM_OF_TREES];
+    Tree<StrengthAndId, int> m_contestantIds[NUM_OF_TREES];
+    Tree<StrengthAndId, int> m_contestantPowers[NUM_OF_TREES];
 
     Team(int id, Sport sport, Country* country);
     Team(const Team& other) = delete;
